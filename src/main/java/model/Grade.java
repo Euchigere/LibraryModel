4e615634f09eeef983945552c77c12aea612de3a
euchigere <ugochigere02@gmail.com>
2020-08-11 00:16:40 +0100

    add grade class

---
+++ src/main/java/model/Grade.java
@@ -0,0 +1,29 @@
+package model;
+
+public enum Grade {
+    CLASS_1("Js1"),
+    CLASS_2("Js2"),
+    CLASS_3("Js3"),
+    CLASS_4("Ss1"),
+    CLASS_5("Ss2"),
+    CLASS_6("Ss3");
+
+    String grade;
+    Grade(String grade) {
+        this.grade = grade;
+    }
+
+    public static Grade valueOfGrade(String grade) {
+        for (Grade g : Grade.values()) {
+            if (g.grade.equals(grade)) {
+                return g;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return grade;
+    }
+}
